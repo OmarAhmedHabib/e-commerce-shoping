38c21f92efef4d447a7d98766755f5814fb74409
OmarAhmedHabib <omarahmedhabib6@gmail.com>
2025-08-10 15:45:51 +0300

    push

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ده بيخلي Vercel يتجاهل أخطاء ESLint أثناء الـ build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
